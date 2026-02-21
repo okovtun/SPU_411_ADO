1f424ed15bdf4985a37b2619c2cc90a740c224af
User <User@DESKTOP-QHG18FL>
2026-02-21 12:34:53 +0300

    Connector.Select start

---
+++ IntroductionToADO/IntroductionToADO/Connector.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace IntroductionToADO
+{
+	class Connector
+	{
+		string connection_string;
+		SqlConnection connection;
+		public Connector(string connection_string)
+		{
+			this.connection_string = connection_string;
+			this.connection = new SqlConnection(connection_string);
+		}
+		public void Select(string cmd)
+		{
+			connection.Open();
+			SqlCommand command = new SqlCommand(cmd, connection);
+			SqlDataReader reader = command.ExecuteReader();
+			while (reader.Read())
+			{
+				//Console.WriteLine($"{reader[0]}\t{reader[1]}\t{reader[2]}");
+				for (int i = 0; i < reader.FieldCount; i++)
+				{
+					Console.Write(reader[i].ToString().PadRight(29));
+				}
+				Console.WriteLine();
+			}
+			reader.Close();
+			connection.Close();
+		}
+	}
+}
---
+++ IntroductionToADO/IntroductionToADO/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace IntroductionToADO
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_SPU_411;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			SqlConnection connection = new SqlConnection(connection_string);
+			string cmd = "SELECT title,year,first_name,last_name FROM Movies JOIN Directors ON(director=director_id)";
+			
+			Connector connector = new Connector(connection_string);
+			connector.Select(cmd);
+		}
+	}
+}
